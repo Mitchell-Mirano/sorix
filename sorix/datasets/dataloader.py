dfcb9e925081bc25e6a45647dc583a3a1cf75493
MitchellMirano <mitchellmirano25@gmail.com>
2026-02-16 19:02:35 -0500

    new datasets and dataloaders

--- sorix/datasets/dataloader.py
+++ sorix/datasets/dataloader.py
@@ -1,17 +1,21 @@
+import numpy as np
+
 class DataLoader:
-    def __init__(self, dataset, batch_size=16):
+    def __init__(self, dataset, batch_size=16, shuffle=True):
         self.dataset = dataset
         self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.indices = np.arange(len(dataset))
 
     def __iter__(self):
+        if self.shuffle:
+            np.random.shuffle(self.indices)
 
-        for i in range(0, len(self.dataset), self.batch_size):
-            yield self.dataset[i:i+self.batch_size]
+        for i in range(0, len(self.indices), self.batch_size):
+            batch_indices = self.indices[i : i + self.batch_size]
+            yield self.dataset[batch_indices]
 
     def __len__(self):
+        return (len(self.dataset) + self.batch_size - 1) // self.batch_size
 
-        ln = len(self.dataset) // self.batch_size
-        if len(self.dataset) % self.batch_size != 0:
-            ln += 1
-        return ln
     
--- sorix/datasets/dataset.py
+++ sorix/datasets/dataset.py
@@ -1,5 +1,7 @@
 class Dataset:
-    def __init__(self, X, y):
+    def __init__(self, X, y=None):
+        if y is not None and len(X) != len(y):
+            raise ValueError(f"X and y must have the same length. Got len(X)={len(X)} and len(y)={len(y)}")
         self.X = X
         self.y = y
 
@@ -7,15 +9,18 @@ class Dataset:
         return len(self.X)
 
     def __getitem__(self, idx):
-
-        return self.X[idx], self.y[idx]
+        if self.y is not None:
+            return self.X[idx], self.y[idx]
+        return self.X[idx]
 
     def __setitem__(self, idx, value):
-        self.X[idx], self.y[idx] = value
-
+        if self.y is not None:
+            self.X[idx], self.y[idx] = value
+        else:
+            self.X[idx] = value
 
     def __str__(self):
-        return f"Dataset(\nX=\n{self.X}, y={self.y})"
+        return f"Dataset(len={len(self)}, has_labels={self.y is not None})"
 
     def __repr__(self):
         return self.__str__()
