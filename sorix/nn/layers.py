70f7c69eea6b709d4f1404fdf08375b9e4f490fa
MitchellMirano <mitchellmirano25@gmail.com>
2026-03-09 00:56:27 -0500

    Kernel fusion for Linear and BatchNorm1d

--- sorix/nn/layers.py
+++ sorix/nn/layers.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import numpy as np
 from typing import Optional, Union, Any
-from sorix.tensor import Tensor, tensor, float32
+from sorix.tensor import Tensor, tensor, float32, _autograd_enabled
 from sorix.cupy.cupy import _cupy_available
 
 if _cupy_available:
@@ -56,9 +56,37 @@ class Linear(Module):
                         device=self.device, requires_grad=True, dtype=float32) if self.bias else None
 
     def __call__(self, X: Tensor) -> Tensor:
-        if self.bias and self.b is not None:
-            return X @ self.W + self.b  
-        return X @ self.W
+        xp = cp if self.device == 'cuda' else np
+        X_data = X.data
+        W_data = self.W.data
+        out_data = X_data @ W_data
+        if self.bias:
+            out_data += self.b.data
+            
+        requires_grad = X.requires_grad or self.W.requires_grad or (self.bias and self.b.requires_grad)
+        
+        if not _autograd_enabled or not requires_grad:
+            return Tensor(out_data, device=self.device, requires_grad=False)
+            
+        deps = [X, self.W]
+        if self.bias:
+            deps.append(self.b)
+            
+        out = Tensor(out_data, deps, 'Linear', device=self.device, requires_grad=True)
+        
+        def _backward() -> None:
+            if out.grad is None:
+                return
+            grad_out = out.grad
+            if X.requires_grad:
+                X._accumulate_grad(grad_out @ W_data.T)
+            if self.W.requires_grad:
+                self.W._accumulate_grad(X_data.T @ grad_out)
+            if self.bias and self.b.requires_grad:
+                self.b._accumulate_grad(xp.sum(grad_out, axis=0, keepdims=True))
+
+        out._backward = _backward
+        return out
     
     def extra_repr(self) -> str:
         return f"in_features={self.W.shape[0]}, out_features={self.W.shape[1]}, bias={self.bias}"
@@ -138,40 +166,46 @@ class BatchNorm1d(Module):
     """
     def __init__(
         self, 
-        features: int, 
-        alpha: float = 0.9, 
-        epsilon: float = 1e-5, 
+        num_features: int, 
+        eps: float = 1e-5, 
+        momentum: float = 0.1, 
         device: str = 'cpu'
     ) -> None:
         super().__init__()
         self.device = device
         xp = cp if device == 'cuda' else np
         
-        self.gamma = tensor(xp.ones((1, features)), requires_grad=True, dtype=float32)
-        self.beta = tensor(xp.zeros((1, features)), requires_grad=True, dtype=float32)
+        self.gamma = tensor(xp.ones((1, num_features)), requires_grad=True, dtype=float32)
+        self.beta = tensor(xp.zeros((1, num_features)), requires_grad=True, dtype=float32)
 
         # buffers (captured by state_dict)
-        self.running_mean = tensor(xp.zeros((1, features)), requires_grad=False, dtype=float32)
-        self.running_var = tensor(xp.ones((1, features)), requires_grad=False, dtype=float32)
+        self.running_mean = tensor(xp.zeros((1, num_features)), requires_grad=False, dtype=float32)
+        self.running_var = tensor(xp.ones((1, num_features)), requires_grad=False, dtype=float32)
 
-        self.alpha = alpha
-        self.epsilon = epsilon
+        self.momentum = momentum
+        self.eps = eps
         self.device = device
         
         if self.device != 'cpu':
             self.to(self.device)
 
     def __call__(self, X: Tensor) -> Tensor:
-        xp = cp if X.device == 'cuda' else np
+        xp = cp if self.device == 'cuda' else np
+        X_data = X.data
+        N = X_data.shape[0]
 
         if self.training:
             # Stats from batch
-            batch_mean = xp.mean(X.data, axis=0, keepdims=True)
-            batch_var = xp.var(X.data, axis=0, keepdims=True)
+            batch_mean = xp.mean(X_data, axis=0, keepdims=True)
+            batch_var = xp.var(X_data, axis=0, keepdims=True)
 
-            # Update buffers
-            self.running_mean.data = self.alpha * self.running_mean.data + (1 - self.alpha) * batch_mean
-            self.running_var.data = self.alpha * self.running_var.data + (1 - self.alpha) * batch_var
+            # Update buffers using PyTorch's running stats formula:
+            # new_val = (1 - momentum) * old_val + momentum * new_val
+            self.running_mean.data = (1 - self.momentum) * self.running_mean.data + self.momentum * batch_mean
+            
+            # PyTorch uses unbiased variance for the running stats (N / (N-1))
+            unbiased_var = batch_var * (N / (N - 1)) if N > 1 else batch_var
+            self.running_var.data = (1 - self.momentum) * self.running_var.data + self.momentum * unbiased_var
 
             mean = batch_mean
             var = batch_var
@@ -181,12 +215,50 @@ class BatchNorm1d(Module):
             var = self.running_var.data
 
         # Normalize
-        X_norm = (X - mean) / xp.sqrt(var + self.epsilon)
-        out = self.gamma * X_norm + self.beta
+        std_inv = 1.0 / xp.sqrt(var + self.eps)
+        X_centered_data = X_data - mean
+        X_norm_data = X_centered_data * std_inv
+        
+        out_data = self.gamma.data * X_norm_data + self.beta.data
+        
+        requires_grad = X.requires_grad or self.gamma.requires_grad or self.beta.requires_grad
+        
+        if not _autograd_enabled or not requires_grad:
+            return Tensor(out_data, device=self.device, requires_grad=False)
+            
+        out = Tensor(out_data, [X, self.gamma, self.beta], 'BatchNorm1d', device=self.device, requires_grad=True)
+        
+        def _backward() -> None:
+            if out.grad is None:
+                return
+            grad_out = out.grad
+            
+            # Gradients w.r.t gamma and beta
+            if self.gamma.requires_grad:
+                self.gamma._accumulate_grad(xp.sum(grad_out * X_norm_data, axis=0, keepdims=True))
+            if self.beta.requires_grad:
+                self.beta._accumulate_grad(xp.sum(grad_out, axis=0, keepdims=True))
+                
+            # Gradient w.r.t X
+            if X.requires_grad:
+                if self.training:
+                    # Analytical derivative of BatchNorm during training
+                    grad_X_norm = grad_out * self.gamma.data
+                    grad_var = xp.sum(grad_X_norm * X_centered_data * -0.5 * (std_inv ** 3), axis=0, keepdims=True)
+                    grad_mean = xp.sum(grad_X_norm * -std_inv, axis=0, keepdims=True) + grad_var * xp.mean(-2.0 * X_centered_data, axis=0, keepdims=True)
+                    
+                    grad_X = (grad_X_norm * std_inv) + (grad_var * 2.0 * X_centered_data / N) + (grad_mean / N)
+                    X._accumulate_grad(grad_X)
+                else:
+                    # Analytical derivative during evaluation modes uses fixed mean/var
+                    grad_X = grad_out * self.gamma.data * std_inv
+                    X._accumulate_grad(grad_X)
+
+        out._backward = _backward
         return out
 
     def extra_repr(self) -> str:
-        return f"features={self.gamma.shape[1]}, eps={self.epsilon}, alpha={self.alpha}"
+        return f"num_features={self.gamma.shape[1]}, eps={self.eps}, momentum={self.momentum}"
 
 class Dropout(Module):
     """
